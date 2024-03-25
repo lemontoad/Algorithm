e72ee1a6585bbaab87cecf67aa49d0d9d62617b0
lemontoad <gimce05@gmail.com>
2024-03-25 20:37:48 +0900

    first

--- baekjoon/1001/1001.c
+++ baekjoon/1001/1001.c
@@ -1,6 +1,33 @@
 #include <stdio.h>
 
 int main(void)
-{
-    printf("hello world");
+{   /*int a;
+    int b;
+   
+    printf("두 숫자 입력: ");
+    scanf("%d %d", &a, &b);
+
+    for(int i; i<10; i++)
+    {
+        printf("%d\n", a + b);
+        a += 1;
+        b *= 2;
+    }
+    */
+    /*
+    printf("int 자료형 크기: %d\n", sizeof(int));
+    printf("float 자료형 크기: %d\n", sizeof(float));
+    printf("double 자료형 크기: %d\n", sizeof(double));
+    printf("char 자료형 크기: %d\n", sizeof(char));
+    printf("long 자료형 크기: %d\n", sizeof(long));
+    printf("long long int 자료형 크기: %d\n", sizeof(long long int));
+    printf("unsigned int 자료형 크기: %d\n", sizeof(unsigned int));
+    */
+
+    
+
+    return 0;
+    // gcc file
+    // ./a.out
+    // cd 파일 위치 ~~~다음
 }
