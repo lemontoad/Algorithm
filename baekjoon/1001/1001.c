49fd115fb52f9004622fad485bf5517f8b9726fc
lemontoad <gimce05@gmail.com>
2024-04-01 20:41:39 +0900

    2

--- baekjoon/1001/1001.c
+++ baekjoon/1001/1001.c
@@ -1,33 +1,93 @@
 #include <stdio.h>
 
 int main(void)
-{   /*int a;
-    int b;
-   
-    printf("두 숫자 입력: ");
-    scanf("%d %d", &a, &b);
+{   
+    /*
+    int arr[5] = {1, 2, 3, 4, 5};
+    printf("arr[%d|%d|%d|%d|%d]\n", arr[0], arr[1], arr[2], arr[3], arr[4]);
 
-    for(int i; i<10; i++)
-    {
-        printf("%d\n", a + b);
-        a += 1;
-        b *= 2;
+    arr[0] = arr[0] * 2;
+    arr[1] = arr[1] * 2;
+    arr[2] = arr[2] * 2;
+    arr[3] = arr[3] * 2;
+    arr[4] = arr[4] * 2;
+
+    printf("arr[%d|%d|%d|%d|%d]\n", arr[0], arr[1], arr[2], arr[3], arr[4]);
+    */
+    /*
+    int num;
+
+    printf("숫자 입력: ");
+    scanf("%d", &num);
+
+    if (num>0) {
+        printf("양수");
+    } else if (num < 0) {
+        printf("음수");
+    } else {
+        printf("0");
     }
     */
     /*
-    printf("int 자료형 크기: %d\n", sizeof(int));
-    printf("float 자료형 크기: %d\n", sizeof(float));
-    printf("double 자료형 크기: %d\n", sizeof(double));
-    printf("char 자료형 크기: %d\n", sizeof(char));
-    printf("long 자료형 크기: %d\n", sizeof(long));
-    printf("long long int 자료형 크기: %d\n", sizeof(long long int));
-    printf("unsigned int 자료형 크기: %d\n", sizeof(unsigned int));
+    int day;
+    printf("요일 번호를 입력하세요 (1~7 순서대로 월~일)");
+    scanf("%d", &day);
+
+    switch (day) {
+       case 1:
+           printf("월요일\n");
+           break;
+       case 2:
+           printf("화요일\n");
+           break;
+       case 3:
+           printf("수요일\n");
+           break;   
+       case 4:
+           printf("목요일\n");
+           break;
+       case 5:
+           printf("금요일\n");
+           break;
+       case 6:
+           printf("토요일\n");
+           break; 
+       case 7:
+           printf("일요일\n");
+       default:
+           printf("잘못된 요일 번호\n");
+           break;
+    }
     */
+    /*
+    int arr[10] = {0,};
+    for (int i = 0; i<=9; i++) {
+        scanf("%d", &arr[i]);
+    }
 
-    
+    for (int i = 0; i<=9; i++) {
+        printf("%d\t", arr[i]);
+    }
+    printf("\n");
+    */
+    /*
+    int num, sum = 0;
 
+    do {
+        printf("숫자를 입력하세요 (0을 입력하면 종료): ");
+        scanf("%d", &num);
+        sum += num;
+    } while (num != 0);
+
+    printf("입력한 숫자들의 합은 %d입니다.\n", sum);
+    */
+    /*
+    int a = 5;
+    int b = ++a; // 전위 증가: a = 6, b = 6
+    int c = a++; // 후위 증가: c = 6, a = 7
+
+    printf("a: %d, b: %d, c: %d\n", a, b, c); // a: 7, b: 6, c: 6
+    */
     return 0;
-    // gcc file
-    // ./a.out
-    // cd 파일 위치 ~~~다음
 }
+//실행: ls, add ., gcc 1001.c, ./a.out
